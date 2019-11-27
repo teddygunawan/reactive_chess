248db25f957faed0d572e1c10f82ab6ed0cb6faa
teddygunawan <teddygunawan55@gmail.com>
2019-11-27 08:44:27 +0700

    Restructure project directory

---
+++ utils/index.js
@@ -0,0 +1,6 @@
+/* Example: */ 
+export {default as global} from './global';
+// Later in other files that need to import global function, do import {global1, global2} from ../utils;
+
+
+/* Example */
